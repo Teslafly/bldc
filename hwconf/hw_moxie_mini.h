82302062381415ae1baa113fa109db3555461b03
Teslafly <mscholz@MAS_Y510P.localdomain>
2020-02-11 01:54:54 -0600

    fixed most io ports and added desccriptions.

--- hwconf/hw_moxie_mini.h
+++ hwconf/hw_moxie_mini.h
@@ -27,6 +27,26 @@
 #define HW_HAS_3_SHUNTS
 #define HW_HAS_PERMANENT_NRF
 
+/*
+* hw info
+*
+
+
+ports:
+- servo, also can act as adc, maybe ws2812 as well w tim5?
+
+- serial1. serial, i2c, and ws2812 timer def supported. either pin.
+- serial2, can only act as serial. connect to bluetooth module
+- i2c1, can also be used as serial port if needed.
+
+- hall, connected to timer, as well as spi 1/3 port pins.
+- analog in / digital. throttle, brake, estop, cruize. 
+	also has dac outputs for debug/dac output.
+	adc's protected.
+*/
+
+
+
 
 // Macros
 #define LED_GREEN_GPIO			GPIOD
@@ -44,15 +64,12 @@
 #define ENABLE_GATE()			palSetrPad(GATE_ENABLE_GPIO, GATE_ENABLE_PIN)
 #define DISABLE_GATE()			palClearPad(GATE_ENABLE_GPIO, GATE_ENABLE_PIN)
 
-#define DCCAL_ON()
+#define DCCAL_ON() // drv spi fcn
 #define DCCAL_OFF()
 #define IS_DRV_FAULT()			(!palReadPad(GPIOB, 7))
 
 
-#define CURRENT_FILTER_ON()		palSetPad(GPIOD, 2)
-#define CURRENT_FILTER_OFF()	palClearPad(GPIOD, 2)
-
-#ifdef HW60_IS_MK3
+#ifdef HW_HAS_SHUTDOWN
 // Shutdown pin
 #define HW_SHUTDOWN_GPIO		GPIOC
 #define HW_SHUTDOWN_PIN			5
@@ -67,17 +84,11 @@
 								PAL_MODE_OUTPUT_PUSHPULL | \
 								PAL_STM32_OSPEED_HIGHEST); \
 								CURRENT_FILTER_ON()
+
+//functions
+bool hw_sample_shutdown_button(void);							
 #else
-// Switch on current filter if a permanent
-// NRF24 cannot be found, as the later
-// HW60 has changed one of the permanent NRF
-// pins to the current filter activation pin.
-#define HW_PERMANENT_NRF_FAILED_HOOK() \
-			palSetPadMode(GPIOD, 2, \
-			PAL_MODE_OUTPUT_PUSHPULL | \
-			PAL_STM32_OSPEED_HIGHEST); \
-			CURRENT_FILTER_ON()
-#endif
+
 
 /*
  * ADC Vector
@@ -97,6 +108,30 @@
  * 12:	Vrefint
  * 13:	IN0		SENS1
  * 14:	IN1		SENS2
+ */
+
+/*
+ * ADC Vector
+ *
+ * 0  (1):	IN0		SENS1
+ * 1  (2):	IN1		SENS2
+ * 2  (3):	IN2		SENS3
+ * 3  (1):	IN10	CURR1
+ * 4  (2):	IN11	CURR2
+ * 5  (3):	IN12	CURR3
+ * 
+ * 6  (1):	IN5		ADC_EXT1
+ * 7  (2):	IN6		ADC_EXT2
+ * 8  (3):	IN3		TEMP_MOS
+ * 9  (1):	IN14	TEMP_MOTOR
+ * 10 (2):	IN15	ADC_EXT3
+ * 11 (3):	IN13	AN_IN
+ * 12 (1):	Vrefint
+ * 13 (2):	IN0		SENS1
+ * 14 (3):	IN1		SENS2
+ * 15 (1):  IN8		TEMP_MOS_2
+ * 16 (2):  IN9		TEMP_MOS_3
+ * 17 (3):  IN3		SENS3
  */
 
 #define HW_ADC_CHANNELS			15
@@ -152,55 +187,68 @@
 // Voltage on ADC channel
 #define ADC_VOLTS(ch)			((float)ADC_Value[ch] / 4096.0 * V_REG)
 
-// Double samples in beginning and end for positive current measurement.
-// Useful when the shunt sense traces have noise that causes offset.
-#ifndef CURR1_DOUBLE_SAMPLE
-#define CURR1_DOUBLE_SAMPLE		0
-#endif
-#ifndef CURR2_DOUBLE_SAMPLE
-#define CURR2_DOUBLE_SAMPLE		0
-#endif
-#ifndef CURR3_DOUBLE_SAMPLE
-#define CURR3_DOUBLE_SAMPLE		0
-#endif
+// // Double samples in beginning and end for positive current measurement.
+// // Useful when the shunt sense traces have noise that causes offset.
+// #ifndef CURR1_DOUBLE_SAMPLE
+// #define CURR1_DOUBLE_SAMPLE		0
+// #endif
+// #ifndef CURR2_DOUBLE_SAMPLE
+// #define CURR2_DOUBLE_SAMPLE		0
+// #endif
+// #ifndef CURR3_DOUBLE_SAMPLE
+// #define CURR3_DOUBLE_SAMPLE		0
+// #endif
 
-// COMM-port ADC GPIOs
+// EXT ADC GPIOs
+// fix
 #define HW_ADC_EXT_GPIO			GPIOA
 #define HW_ADC_EXT_PIN			5
 #define HW_ADC_EXT2_GPIO		GPIOA
 #define HW_ADC_EXT2_PIN			6
 
 // UART Peripheral
-#define HW_UART_DEV				SD3
-#define HW_UART_GPIO_AF			GPIO_AF_USART3
+// *good to moxie?
+// test
+#define HW_UART_DEV				SD1
+#define HW_UART_GPIO_AF			GPIO_AF_USART1
 #define HW_UART_TX_PORT			GPIOB
-#define HW_UART_TX_PIN			10
+#define HW_UART_TX_PIN			6
 #define HW_UART_RX_PORT			GPIOB
-#define HW_UART_RX_PIN			11
+#define HW_UART_RX_PIN			7
 
-#ifdef HW60_IS_MK3
 // Permanent UART Peripheral (for NRF51)
+// *good to moxie
 #define HW_UART_P_BAUD			115200
 #define HW_UART_P_DEV			SD4
-#define HW_UART_P_DEV_TX		SD5 // UART for TX, due to mistake below
 #define HW_UART_P_GPIO_AF		GPIO_AF_UART4
 #define HW_UART_P_TX_PORT		GPIOC
-#define HW_UART_P_TX_PIN		12 // This is a mistake in the HW. We have to use a hack to use UART5.
+#define HW_UART_P_TX_PIN		10
 #define HW_UART_P_RX_PORT		GPIOC
 #define HW_UART_P_RX_PIN		11
-#endif
 
-// ICU Peripheral for servo decoding
-#define HW_USE_SERVO_TIM4
-#define HW_ICU_TIMER			TIM4
-#define HW_ICU_TIM_CLK_EN()		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE)
-#define HW_ICU_DEV				ICUD4
-#define HW_ICU_CHANNEL			ICU_CHANNEL_1
-#define HW_ICU_GPIO_AF			GPIO_AF_TIM4
-#define HW_ICU_GPIO				GPIOB
-#define HW_ICU_PIN				6
+// !!!!!!!!!! test
+// ICU Peripheral for servo decoding (from unity conf, test.)
+// servo actually on PA3, tim9/ch2, or tim5/ch4
+#define HW_ICU_TIMER            TIM9
+#define HW_ICU_DEV              ICUD9
+//HW_ICU_TIM_CLK_EN() need to bring this over?
+#define HW_ICU_CHANNEL          ICU_CHANNEL_2
+#define HW_ICU_GPIO_AF          GPIO_AF_TIM9
+#define HW_ICU_GPIO             GPIOA
+#define HW_ICU_PIN 	            3
+
+// // ICU Peripheral for servo decoding
+// #define HW_USE_SERVO_TIM4
+// #define HW_ICU_TIMER			TIM4
+// #define HW_ICU_TIM_CLK_EN()		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE)
+// #define HW_ICU_DEV				ICUD4
+// #define HW_ICU_CHANNEL			ICU_CHANNEL_1
+// #define HW_ICU_GPIO_AF			GPIO_AF_TIM4
+// #define HW_ICU_GPIO				GPIOB
+// #define HW_ICU_PIN				6
 
 // I2C Peripheral
+// *good to moxie
 #define HW_I2C_DEV				I2CD2
 #define HW_I2C_GPIO_AF			GPIO_AF_I2C2
 #define HW_I2C_SCL_PORT			GPIOB
@@ -209,6 +257,7 @@
 #define HW_I2C_SDA_PIN			11
 
 // Hall/encoder pins
+// *good to moxie
 #define HW_HALL_ENC_GPIO1		GPIOC
 #define HW_HALL_ENC_PIN1		6
 #define HW_HALL_ENC_GPIO2		GPIOC
@@ -226,65 +275,53 @@
 #define HW_ENC_TIM_ISR_CH		TIM3_IRQn
 #define HW_ENC_TIM_ISR_VEC		TIM3_IRQHandler
 
-#ifndef HW60_IS_MK3
-// NRF pins
-#define NRF_PORT_CSN			GPIOB
-#define NRF_PIN_CSN				12
-#define NRF_PORT_SCK			GPIOB
-#define NRF_PIN_SCK				4
-#define NRF_PORT_MOSI			GPIOB
-#define NRF_PIN_MOSI			3
-#define NRF_PORT_MISO			GPIOD
-#define NRF_PIN_MISO			2
-#endif
+// // SPI pins
+// // connected to hall port for direct spi encoder
+// #define HW_SPI_DEV				SPID1
+// #define HW_SPI_GPIO_AF			GPIO_AF_SPI1
+// #define HW_SPI_PORT_NSS			GPIOA
+// #define HW_SPI_PIN_NSS			4
+// #define HW_SPI_PORT_SCK			GPIOA
+// #define HW_SPI_PIN_SCK			5
+// #define HW_SPI_PORT_MOSI		GPIOA
+// #define HW_SPI_PIN_MOSI			7
+// #define HW_SPI_PORT_MISO		GPIOA
+// #define HW_SPI_PIN_MISO			6
 
-// SPI pins
-#define HW_SPI_DEV				SPID1
-#define HW_SPI_GPIO_AF			GPIO_AF_SPI1
-#define HW_SPI_PORT_NSS			GPIOA
-#define HW_SPI_PIN_NSS			4
-#define HW_SPI_PORT_SCK			GPIOA
-#define HW_SPI_PIN_SCK			5
-#define HW_SPI_PORT_MOSI		GPIOA
-#define HW_SPI_PIN_MOSI			7
-#define HW_SPI_PORT_MISO		GPIOA
-#define HW_SPI_PIN_MISO			6
-
-// SPI for DRV8301
-#ifndef HW60_IS_MK3
+// SPI for DRV8353
+// *good to moxie? no
+// todo: change to drv8353
+// mosi = pb1
+// miso = pb12
+// sck = pc13
+// cs =  pc14
 #define DRV8301_MOSI_GPIO		GPIOB
-#define DRV8301_MOSI_PIN		4
+#define DRV8301_MOSI_PIN		1
 #define DRV8301_MISO_GPIO		GPIOB
-#define DRV8301_MISO_PIN		3
+#define DRV8301_MISO_PIN		12
 #define DRV8301_SCK_GPIO		GPIOC
-#define DRV8301_SCK_PIN			10
+#define DRV8301_SCK_PIN			13
 #define DRV8301_CS_GPIO			GPIOC
-#define DRV8301_CS_PIN			9
-#else
-#define DRV8301_MOSI_GPIO		GPIOC
-#define DRV8301_MOSI_PIN		12
-#define DRV8301_MISO_GPIO		GPIOC
-#define DRV8301_MISO_PIN		11
-#define DRV8301_SCK_GPIO		GPIOC
-#define DRV8301_SCK_PIN			10
-#define DRV8301_CS_GPIO			GPIOC
-#define DRV8301_CS_PIN			9
-#endif
+#define DRV8301_CS_PIN			14
 
-// MPU9250
-#define MPU9X50_SDA_GPIO		GPIOB
-#define MPU9X50_SDA_PIN			2
-#define MPU9X50_SCL_GPIO		GPIOA
-#define MPU9X50_SCL_PIN			15
-#define IMU_FLIP
 
-#ifdef HW60_IS_MK3
-// NRF SWD
-#define NRF5x_SWDIO_GPIO		GPIOB
-#define NRF5x_SWDIO_PIN			12
-#define NRF5x_SWCLK_GPIO		GPIOA
-#define NRF5x_SWCLK_PIN			4
-#endif
+// // MPU9250
+// // fix to i2c
+// #ifdef HW_HAS_MPU9250
+// #define MPU9X50_SDA_GPIO		GPIOB
+// #define MPU9X50_SDA_PIN			2
+// #define MPU9X50_SCL_GPIO		GPIOA
+// #define MPU9X50_SCL_PIN			15
+// //#define IMU_FLIP
+// #endif
+
+// #ifdef HW_HAS_NRF_SWD
+// // NRF SWD
+// #define NRF5x_SWDIO_GPIO		GPIOB
+// #define NRF5x_SWDIO_PIN			12
+// #define NRF5x_SWCLK_GPIO		GPIOA
+// #define NRF5x_SWCLK_PIN			4
+// #endif
 
 // Measurement macros
 #define ADC_V_L1				ADC_Value[ADC_IND_SENS1]
@@ -302,25 +339,20 @@
 #define MCCONF_DEFAULT_MOTOR_TYPE		MOTOR_TYPE_FOC
 #endif
 #ifndef MCCONF_L_MAX_ABS_CURRENT
-#define MCCONF_L_MAX_ABS_CURRENT		150.0	// The maximum absolute current above which a fault is generated
+#define MCCONF_L_MAX_ABS_CURRENT		100.0	// The maximum absolute current above which a fault is generated
 #endif
 #ifndef MCCONF_FOC_SAMPLE_V0_V7
 #define MCCONF_FOC_SAMPLE_V0_V7			false	// Run control loop in both v0 and v7 (requires phase shunts)
 #endif
 
 // Setting limits
-#define HW_LIM_CURRENT			-120.0, 120.0
-#define HW_LIM_CURRENT_IN		-120.0, 120.0
+#define HW_LIM_CURRENT			-110.0, 110.0
+#define HW_LIM_CURRENT_IN		-40.0, 60.0
 #define HW_LIM_CURRENT_ABS		0.0, 160.0
-#define HW_LIM_VIN				6.0, 57.0
-#define HW_LIM_ERPM				-200e3, 200e3
-#define HW_LIM_DUTY_MIN			0.0, 0.1
-#define HW_LIM_DUTY_MAX			0.0, 0.99
-#define HW_LIM_TEMP_FET			-40.0, 110.0
-
-// Functions
-#ifdef HW60_IS_MK3
-bool hw_sample_shutdown_button(void);
-#endif
+#define HW_LIM_VIN				11.0, 70.0
+#define HW_LIM_ERPM				-100e3, 100e3
+#define HW_LIM_DUTY_MIN			0.0, 0.5
+#define HW_LIM_DUTY_MAX			0.0, 0.95
+#define HW_LIM_TEMP_FET			-40.0, 90.0
 
 #endif /* HW_60_H_ */
