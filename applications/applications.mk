2cf42fef483914ef6a356706ebf7f933262cfd7e
teslafly <2079881+Teslafly@users.noreply.github.com>
2021-04-09 00:02:34 -0500

    Merge branch 'master' of https://github.com/vedderb/bldc into gearbox_servo_app_dev

--- applications/applications.mk
+++ applications/applications.mk
@@ -5,7 +5,8 @@ APPSRC =	applications/app.c \
 			applications/app_uartcomm.c \
 			applications/app_nunchuk.c \
 			applications/app_balance.c \
+			applications/app_pas.c \
 			applications/app_gearbox_servo.c \
-
+			# applications/app_custom.c
 
 APPINC = applications
